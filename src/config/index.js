e7f93573ad4da95ec13563dfa80a678d019dff6c
YuTingtao <735126858@qq.com>
2022-04-10 09:57:09 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -5,12 +5,15 @@ import prod from './prod.js'
 let config
 
 if (process.env.NODE_ENV === 'production') {
-    switch (process.env.VUE_APP_FLAG) {
+    switch (process.env.VUE_APP_ENV) {
         case 'prod':
             config = prod
             break;
         case 'test':
             config = test
+            break;
+        case 'dev':
+            config = dev
             break;
         default:
             break;
