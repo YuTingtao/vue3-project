b15cf0302057204ea595d7286f65348522c0d6c5
YuTingtao <735126858@qq.com>
2022-10-01 14:08:22 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,18 +19,19 @@ module.exports = {
     },
     plugins: ['vue', '@typescript-eslint'],
     rules: {
-        'indent': ['error', 4],
-        'max-len': [
-            'error',
-            {
-                'code': 100,
-                'tabWidth': 4
-            }
-        ],
-        'semi': ['error', 'never'],
-        'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
-        'no-unused-vars': 'off',
-        'object-curly-spacing': ['error', 'always'],
+        'indent': ['error', 4],                               // 缩进4个空格
+        'vue/html-indent': ['error', 4],
+        'max-len': ['error', { 'code': 100, 'tabWidth': 4 }], // 一行最大100长度
+        'semi': ['error', 'never'],                           // js结尾不加分号
+        'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // js单引号
+        'camelcase': 'error',                                 // 驼峰命名
+        'comma-dangle': ['error', 'never'],                   // 禁止使用拖尾逗号
+        'comma-spacing': ['error', { 'before': false, 'after': true }], // 逗号后面带一个空格
+        'object-curly-spacing': ['error', 'always'],          // 对象括号内带空格
+        'no-multiple-empty-lines': ['error', { 'max': 2 }],   // 最大2空行
+        'no-unused-vars': 'off',                              // 未使用变量不报错
+        'vue/no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
         'vue/multi-word-component-names': 'off'
     }
 }
