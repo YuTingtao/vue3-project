d3688393bd84908cfc0e844c86cefb29d8a3834c
YuTingtao <735126858@qq.com>
2023-03-19 13:58:40 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,6 @@ import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
 // 监听script报错
 document.querySelector('script').addEventListener('error', function(e) {
-  console.log('App:', e)
+  console.log('App监听到的错误:', e)
 })
 </script>
