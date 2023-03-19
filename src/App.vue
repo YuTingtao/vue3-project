16aa5875f8965ccf9cdce76cd400bdb4fd14c70e
YuTingtao <735126858@qq.com>
2023-03-19 15:35:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,26 @@
 <script setup>
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
+// 获取版本信息
+function getVersion() {
+  var xhr = new XMLHttpRequest()
+  xhr.open('get', '/version.json', true)
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var res = JSON.parse(xhr.responseText)
+      var version = localStorage.appVersion || ''
+      if (res.version != version) {
+        localStorage.appVersion = res.version
+        location.reload()
+      }
+    }
+  }
+  xhr.send()
+}
+
 // 监听script报错
-window.addEventListener('unhandledrejection', function(e) {
-  console.log('App监听到的错误:', e)
+window.addEventListener('error', function(e) {
+  console.log('错误:', e)
+  getVersion()
 })
 </script>
