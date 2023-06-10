11f26305702989d2abc70cc35a68487160f2f70d
YuTingtao <735126858@qq.com>
2023-06-10 21:56:17 +0800

    update

--- src/directives/index.js
+++
@@ -1,9 +0,0 @@
-import error from './modules/error.js'
-import permission from './modules/permission.js'
-
-function importDirectives(app) {
-  app.directive('error', error)
-  app.directive('permission', permission)
-}
-
-export default importDirectives
--- src/layout/LayoutEmpty.vue
+++
@@ -1,4 +0,0 @@
-<!-- 2层以上路由嵌套使用 -->
-<template>
-  <router-view></router-view>
-</template>
