83c8266cdb12f1671bfa4b77fb19c9cf0a96b664
YuTingtao <735126858@qq.com>
2023-06-24 10:39:37 +0800

    update

--- version.js
+++ version.js
@@ -1,15 +1,15 @@
 // 生成版本JOSN文件
-import fs from 'fs'
+const fs = require('node:fs')
 
 function initVersion() {
-  const obj = {
+  let data = JSON.stringify({
     version: 'v_' +  Date.now()
-  }
-  fs.writeFile('./docs/version.json', JSON.stringify(obj), err => {
+  })
+  fs.writeFile('./docs/version.json', data, err => {
     if (err) {
       console.log('生成version文件失败:', err)
     } else {
-      console.log('生成version文件:', JSON.stringify(obj))
+      console.log('生成version文件:', data)
     }
   })
 }
