8a448230582b8679ac354f856c4cabf0bbbfd007
YuTingtao <735126858@qq.com>
2023-09-26 23:08:47 +0800

    update

--- version.js
+++ version.js
@@ -5,11 +5,11 @@ function initVersion() {
   let data = JSON.stringify({
     version: 'v_' +  Date.now()
   })
-  fs.writeFile('./docs/version.json', data, err => {
+  fs.writeFile('./public/version.json', data, err => {
     if (err) {
       console.log('生成version文件失败:', err)
     } else {
-      console.log('生成version文件:', data)
+      console.log('生成version文件成功:', data)
     }
   })
 }
