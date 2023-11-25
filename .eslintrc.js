2bc2223bd005275e7aa57ccf4ff2f9d826df2e22
YuTingtao <735126858@qq.com>
2023-11-25 23:52:31 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
     'comma-style': ['error', 'last'],                     // 逗号放置在当前行末尾
     'func-call-spacing': ['error', 'never'],              // 函数调用时括号前不带空格
     'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }], // 使用函数声明定义函数
-    'max-len': ['error', { 'code': 120, 'tabWidth': 4 }], // 一行最大120长度
+    'max-len': ['error', { 'code': 120, 'tabWidth': 2 }], // 一行最大120长度
     'new-cap': ['error', { 'capIsNew': true }],           // 构造函数名首字母大写
     'spaced-comment': ['error', 'always'],                // 注释斜杠后面加空格
     'object-curly-spacing': ['error', 'always'],          // 对象括号内带空格
