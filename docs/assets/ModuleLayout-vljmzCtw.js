cfd419c48d708f90c3a28eb3462e1fd7665eed10
yutingtao <735126858@qq.com>
2024-06-16 13:08:58 +0800

    清空

--- docs/assets/ModuleLayout-vljmzCtw.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{aa as r,o as t,W as c}from"./@vue-iFnbdo9x.js";const n={};function a(s,_){const o=r("router-view");return t(),c(o)}const p=e(n,[["render",a]]);export{p as default};
--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-config-provider :locale="zhCn">
-    <router-view></router-view>
-  </el-config-provider>
-</template>
-
-<script setup>
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-</script>
