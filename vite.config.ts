f3f5e6847c7a52d0008e608d8c6ca1b8f4082c17
余廷涛 <735126858@qq.com>
2025-01-08 23:06:06 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -90,6 +90,9 @@ export default defineConfig({
     chunkSizeWarningLimit: 1024 * 500,
     rollupOptions: {
       output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'assets/js/[name]-[hash].js', // 包的入口文件名称
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]', // 资源文件像：字体、图片等
         // manualChunks: (id) => {
         //   if(id.includes('node_modules')) {
         //     // return id.toString().split('node_modules/')[1].split('/')[0].toString()
