b3221d22b58c64d1543cecc1155c78e74bd9aff3
余廷涛 <735126858@qq.com>
2025-02-28 19:58:15 +0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     ElMessage: true
   },
   overrides: [],
-  parser: 'vue-eslint-parser',
+  parser: ['vue-eslint-parser', '@typescript-eslint/parser'],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
@@ -41,9 +41,8 @@ module.exports = {
     'no-unused-vars': 'off',                              // 未使用变量不报错
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any':'off',
-    'typescript/triple-slash-reference': 'off',           // 关闭三斜线指令的报错
     'vue/html-indent': ['error', 2, { 'alignAttributesVertically': false }],
     'vue/multi-word-component-names': 'off',
     'vue/no-unused-vars': 'off',
   }
-}
+};
