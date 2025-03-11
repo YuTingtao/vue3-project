17ead3fc82d73f597e90c31fc0730be3e811e348
YuTingtao <735126858@qq.com>
2025-03-11 09:12:07 +0800

    update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
-// / <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
-  // 更多环境变量...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
+/// <reference types="vite/client" />
