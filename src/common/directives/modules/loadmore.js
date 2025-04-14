198cfa73d6280bb4c4e997a2db3e28fdab2977ff
YuTingtao <735126858@qq.com>
2025-04-14 13:41:36 +0800

    update

--- src/common/directives/modules/loadmore.js
+++ src/common/directives/modules/loadmore.js
@@ -2,18 +2,18 @@
 export default {
   mounted(el, binding) {
     let dom = el;
-    let callback = binding.value;
+    let method = binding.value;
 
     if (typeof binding.value === 'object') {
-      dom = document.querySelector(binding.value.trigger);
-      callback = binding.value.callback;
+      dom = document.querySelector(binding.value.el);
+      method = binding.value.method;
     }
     if (!dom) return;
 
     dom.addEventListener('scroll', () => {
       const { scrollTop, scrollHeight, clientHeight } = dom;
       if (scrollTop + clientHeight >= scrollHeight) {
-        callback();
+        method();
       }
     });
   }
--- src/views/example/base/index.vue
+++ src/views/example/base/index.vue
@@ -21,11 +21,14 @@
     </div>
 
     <div class="row-box">
-      <span>加载更多指令：</span>
-      <el-select style="width: 220px;" v-model="selected"
-        v-loadmore="{ trigger: '.select-loadmore .el-select-dropdown__wrap', callback: getOptions }" popper-class="select-loadmore"
-        placeholder="请选择" clearable>
-        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+      <span>v-loadmore指令：</span>
+      <el-select style="width: 220px;"
+        v-model="selected" placeholder="请选择" clearable
+        v-loadmore="{ el: '.select-loadmore .el-select-dropdown__wrap', method: getOptions }"
+        popper-class="select-loadmore">
+        <el-option
+          v-for="item in options" :key="item.value"
+          :label="item.label" :value="item.value">
         </el-option>
       </el-select>
     </div>
@@ -49,11 +52,12 @@ const urlList = ref([
 ]);
 
 // 加载更多指令
-const selectEl = ref();
 const selected = ref();
 const options = ref<{ label: string; value: number }[]>([]);
 function getOptions() {
-  console.log('getOptions');
+  if (options.value.length >= 100) {
+    return false;
+  }
   for (let i = 0; i < 10; i++) {
     const random = Math.floor(Math.random() * 100);
     options.value.push({
