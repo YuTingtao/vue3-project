945b20252122e1375114c996639867b5d6d79147
YuTingtao <735126858@qq.com>
2025-10-15 15:12:07 +0800

    update

--- src/views/example/LargeFileUpload.vue
+++ src/views/example/LargeFileUpload.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="large-file-upload">
-    <el-upload v-model:file-list="fileList" action="#" :auto-upload="false" :on-change="onChange">
+    <el-upload v-model:file-list="fileList" action="#" :multiple="true" :http-request="httpRequest">
       <template #trigger>
-        <el-button type="primary" :disabled="uploading">选择文件</el-button>
+        <el-button type="primary">选择文件</el-button>
       </template>
-      <span class="upload-progress" v-show="uploading">上传中{{ uploadProgress }}%...</span>
       <template #tip>
         <div class="el-upload__tip">文件大小不能超过2G</div>
       </template>
@@ -13,31 +12,26 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { reactive, ref } from 'vue';
 import axios from 'axios';
-import type { UploadFile, UploadUserFile } from 'element-plus';
+import type { UploadProgressEvent, UploadRequestOptions, UploadUserFile } from 'element-plus';
 import { runPool } from '@/utils/index.ts';
 
 interface FileChunk {
-  file: Blob; // 切片文件
-  progressLoaded: number; // 已上传字节数
+  [key: number]: {
+    chunks: {
+      file: Blob; // 切片文件
+      progressLoaded: number; // 已上传字节数
+    }[];
+    hash: string; // 切片hash
+  };
 }
-
-// 文件切片列表
-const fileChunks = ref<FileChunk[]>([]);
 
 // 文件列表
 const fileList = ref<UploadUserFile[]>([]);
 
-// 文件hash
-const hash = ref('');
-
-// 上传进度
-const uploadProgress = computed(() => {
-  if (!fileList.value[0]?.size) return 0;
-  const uploaded = fileChunks.value.reduce((acc, chunk) => acc + chunk.progressLoaded, 0);
-  return Math.floor((uploaded / fileList.value[0].size) * 100);
-});
+// 文件列表切片对象
+const fileChunk: FileChunk = reactive({});
 
 /**
  * 创建文件切片
@@ -58,11 +52,11 @@ function createFileChunk(file: File, size = 1024 * 1024 * 10) {
 }
 
 // 获取文件hash
-function getFileHash() {
+function getFileHash(key: number) {
   return new Promise((resolve) => {
     const worker = new Worker('/js/hash.js');
     const data = {
-      fileChunks: fileChunks.value.map((item) => item.file)
+      fileChunks: fileChunk[key].chunks.map((item) => item.file)
     };
     worker.postMessage(data);
     worker.onmessage = (e) => {
@@ -75,66 +69,102 @@ function getFileHash() {
   });
 }
 
-const uploading = ref(false);
-
-// 上传切片
-async function uploadChunks() {
-  uploading.value = true;
-  hash.value = (await getFileHash()) as string;
-  const uploadList = fileChunks.value.map((item, index) => {
-    const formData = new FormData();
-    formData.append('fileName', fileList.value[0].name);
-    formData.append('file', item.file);
-    formData.append('hash', `${hash.value}-${index}`);
-    return () =>
-      axios({
-        url: '/api/upload/uploadChunk',
-        method: 'POST',
-        headers: { 'Content-Type': 'multipart/form-data' },
-        data: formData,
-        onUploadProgress: (progressEvent) => {
-          if (progressEvent?.total) {
-            item.progressLoaded = progressEvent.loaded;
-          }
-        }
-      });
-  });
-  await runPool(uploadList, 5).catch((errs) => {
-    ElMessage.error('上传失败');
-    uploading.value = false;
-    fileList.value = [];
-  });
-  await mergeChunksRequst().catch((err) => {
-    uploading.value = false;
-    fileList.value = [];
-  });
-  uploading.value = false;
+// 上传进度
+function getUploadProgress(uid: number) {
+  const index = fileList.value.findIndex((item) => item.uid === uid);
+  const uploaded = fileChunk[uid].chunks.reduce((acc, chunk) => acc + chunk.progressLoaded, 0);
+  if (!fileList.value[index]?.size) return 100;
+  const size = fileList.value[index].size;
+  return Math.floor((uploaded / size) * 100);
 }
 
 // 合并切片请求
-async function mergeChunksRequst() {
+async function mergeChunksRequst(uid: number) {
   const res = await axios.post('/api/upload/merge', {
     fileName: fileList.value[0].name,
-    hash: hash.value
+    hash: fileChunk[uid].hash
   });
   if (res.status === 200) {
     fileList.value[0].url = res.data?.url || '';
   }
 }
 
-// 监听文件选择
-async function onChange(uploadFile: UploadFile) {
-  // console.log('文件:', uploadFile);
-  fileList.value = [{ name: uploadFile.name, size: uploadFile.size }];
-  fileChunks.value = createFileChunk(uploadFile.raw as File);
-  await uploadChunks();
+// 上传切片
+async function uploadChunks(options: UploadRequestOptions) {
+  const uid = options.file.uid;
+  const hash = fileChunk[uid].hash;
+  const uploadList = fileChunk[uid].chunks.map((item, index) => {
+    const formData = new FormData();
+    formData.append('fileName', fileList.value[0].name);
+    formData.append('file', item.file);
+    formData.append('hash', `${hash}-${index}`);
+    return () =>
+      axios({
+        url: '/api/upload/uploadChunk',
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        data: formData,
+        onUploadProgress: (e) => {
+          if (e?.total) {
+            item.progressLoaded = e.loaded;
+          }
+          const evt = {
+            ...e,
+            percent: getUploadProgress(uid)
+          };
+          options.onProgress(evt as unknown as UploadProgressEvent);
+        }
+      });
+  });
+  const resArr = await runPool(uploadList, 5).catch((errs) => {
+    options.onError(errs);
+    ElMessage.error('上传失败');
+  });
+  if (resArr) {
+    await mergeChunksRequst(uid)
+      .then((res) => {
+        options.onSuccess(res);
+      })
+      .catch((err) => {
+        options.onError(err);
+        ElMessage.error('上传失败');
+      });
+  }
+}
+
+async function httpRequest(options: UploadRequestOptions) {
+  // console.log('options', options);
+  const { file } = options;
+  fileChunk[file.uid] = {
+    chunks: [],
+    hash: ''
+  };
+  fileChunk[file.uid].chunks = createFileChunk(file);
+  fileChunk[file.uid].hash = (await getFileHash(file.uid)) as string;
+  uploadChunks(options);
 }
 </script>
 
 <style lang="scss">
 .large-file-upload {
-  .upload-progress {
-    margin-left: 10px;
+  .el-upload-list__item {
+    margin-bottom: 0;
+  }
+  .el-upload-list__item .el-progress__text {
+    margin-bottom: 0;
+  }
+  .el-upload-list--text {
+    line-height: 30px;
+  }
+  .el-upload-list__item .el-upload-list__item-info {
+    margin-left: 0;
+  }
+  .el-upload-list__item .el-progress {
+    top: 30px;
+    margin-top: 0 !important;
+  }
+  .el-upload-list__item .el-progress__text {
+    top: -19px;
   }
 }
 </style>
