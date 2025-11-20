4e3c96b56d0c2572f0112c8bffa7c489d22de180
YuTingtao <735126858@qq.com>
2025-11-20 13:54:24 +0800

    update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,21 @@
-// eslint-disable-next-line spaced-comment
+/* eslint-disable spaced-comment */
 /// <reference types="vite/client" />
 
-declare module 'virtual:svg-icons-register';
+// 定义vue全局属性的类型
+// export {};
+
+// declare module 'vue' {
+//   interface ComponentCustomProperties {
+//     $hasPermission: (permission: string, route?: string) => boolean;
+//   }
+// }
+
+// 定义vue-router路由元信息的类型
+// declare module 'vue-router' {
+//   interface RouteMeta {
+//     title?: string;
+//     icon?: string;
+//     activePath?: string;
+//     parentPath?: string;
+//   }
+// }
