0abab171e8eefd866053f18bbcb3ae765040822c
YuTingtao <735126858@qq.com>
2025-12-25 10:22:44 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>{{ title }}</div>
+  <div class="page-intro">
+    <div class="intro">{{ title }}</div>
+    <AppPagination :total="100"></AppPagination>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,4 +11,17 @@ import { ref } from 'vue';
 const title = ref('Vue3 技术栈基础模板');
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page-intro {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.intro {
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 50px;
+}
+</style>
