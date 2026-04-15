f10129d570dae558e5e84cc15ef117e6106cf67f
YuTingtao <735126858@qq.com>
2026-04-15 15:34:28 +0000

    Deploying to gh-pages from @ YuTingtao/vue3-project@0e808c229081a4f6be8ac583aeeaf3f8cd8c00fc 🚀

---
+++ assets/js/permission-DMHvJBZZ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./store-Dgz9aClZ.js";import{t}from"./router-Co9Z_Bzi.js";var n=t.currentRoute;function r(){let t=e();function r(e,r=n.value.name||n.value.path){var i;return!!(((i=t.menuObj[r])==null||(i=i.meta)==null?void 0:i.buttons)||[]).some(t=>t.name===e)}return{hasPermission:r}}export{r as t};
